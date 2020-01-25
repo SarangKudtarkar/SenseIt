95b888b7b8da95a4c51f04d64b1eacb73d976c1e
Sarang <sarangkudtarkar49@gmail.com>
2020-01-25 12:40:47 +0530

    Study Activity states

--- app/src/main/java/com/example/android/senseit/MainActivity.java
+++ app/src/main/java/com/example/android/senseit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.senseit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Activity : ","created");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Activity : ","started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Activity : ","stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Activity : ","destroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Activity : ","paused");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Activity : ","resumed");
     }
 }
