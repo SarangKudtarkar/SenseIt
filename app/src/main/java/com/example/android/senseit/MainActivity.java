bbb8e2639c8dd0205e2b58d9675a66bc1e3f6e8e
Sarang <sarangkudtarkar49@gmail.com>
2020-01-30 22:35:45 +0530

    Magnetometer added

--- app/src/main/java/com/example/android/senseit/MainActivity.java
+++ app/src/main/java/com/example/android/senseit/MainActivity.java
@@ -1,45 +1,89 @@
 package com.example.android.senseit;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaRecorder;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    public static final int  RECORD_AUDIO=0;
+private SensorManager sensormanager;
+public static DecimalFormat decimalFormatter;
+    private TextView reading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Activity : ","created");
-    }
+        //Show welcome screen
+        DecimalFormatSymbols decimalFormatSymbols=new DecimalFormatSymbols(Locale.US);
+        decimalFormatSymbols.setDecimalSeparator('.');
+        decimalFormatter=new DecimalFormat("#.000",decimalFormatSymbols);
+        sensormanager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        reading=findViewById(R.id.textView);
+        reading.setVisibility(View.GONE);
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Welcome", Snackbar.LENGTH_LONG);
+        snackbar.show();
+        //ask for permission
+        Button soundMeasure=findViewById(R.id.button_sound);
+        soundMeasure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+reading.setVisibility(View.VISIBLE);
+            }
+        });
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("Activity : ","started");
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("Activity : ","stop");
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("Activity : ","destroy");
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("Activity : ","paused");
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("Activity : ","resumed");
+        sensormanager.registerListener(this,sensormanager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),sensormanager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensormanager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD)
+{
+    float magx,magy,magz;
+    magx=event.values[0];
+    magy=event.values[1];
+    magz=event.values[2];
+
+    double magnitude=Math.sqrt((magx*magx)+(magy*magy)+(magz*magz));
+reading.setText("The current value of magnetic field is "+magnitude);
+}
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
